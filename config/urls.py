8486a525361c66de465cf34310c46db77d91a60e
Turvi Nehete <turvinehete@gmail.com>
2026-07-27 13:10:04 +0530

    Redirect root URL to login

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,16 @@
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("core.urls")),
-    #path('api/',include("core.urls")),
+    path(
+        "",
+        RedirectView.as_view(
+            url="/login/",
+            permanent=False
+        ),
+    ),
 
-    #path("login/", views.login, name="login"),
-    #path("register/", views.register, name="register"),
-    #path("dashboard/", views.dashboard, name="dashboard"),
-    #path("clients/", views.clients, name="clients"),
-    #path("campaigns/", views.campaigns, name="campaigns"), 
-    #path("ai_chat/",views.ai_chat,name="ai_chat"),
-    #path("meetings/",views.meetings,name="meetings"),
-    #path("audit/",views.audit,name="audit"),
-    #path("templates/",views.templates,name="templates"),
-
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
-
-# from django.contrib import admin
-# from django.urls import path,include
-# from . import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('',include('core.urls')),
-
-# ]
-
-
-
